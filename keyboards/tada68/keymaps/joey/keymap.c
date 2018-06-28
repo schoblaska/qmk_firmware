103edadb21d74f4190a3ba84794f292aa080c4f0
Joey Schoblaska <joey.schoblaska@gmail.com>
2018-06-28 14:32:18 -0500

    fixed tmux pane macros

--- keyboards/tada68/keymaps/joey/keymap.c
+++ keyboards/tada68/keymaps/joey/keymap.c
@@ -10,6 +10,11 @@
 
 #define _______ KC_TRNS
 
+enum custom_keycodes {
+  CTAO = SAFE_RANGE,
+  CTASC
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = KEYMAP_ANSI(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_MPLY,  \
@@ -21,8 +26,8 @@ LT(1, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC
 
   [1] = KEYMAP_ANSI(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,  \
-      _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, M(0),    _______, _______, _______, _______, KC_MPRV,  \
-      _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, M(1),    _______,          _______, KC_MUTE,  \
+      _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, CTAO,    _______, _______, _______, _______, KC_MPRV,  \
+      _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, CTASC,   _______,          _______, KC_MUTE,  \
       _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2,  \
       _______, _______, _______,                   _______,                            _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
   )
@@ -32,14 +37,16 @@ const uint16_t PROGMEM fn_actions[] = {
 	[0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
-    switch(id) {
-      case 0:
-        return MACRO(D(LCTL), T(A), U(LCTL), T(O));
-      case 1:
-        return MACRO(D(LCTL), T(A), U(LCTL), T(SCLN));
+    switch(keycode) {
+      case CTAO:
+        SEND_STRING(SS_LCTRL("a")"o");
+        return false;
+      case CTASC:
+        SEND_STRING(SS_LCTRL("a")";");
+        return false;
     }
   }
-  return MACRO_NONE;
+  return true;
 };
