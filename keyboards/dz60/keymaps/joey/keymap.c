e42a021452a873feeeb63fcce4b75fc07fa1db40
Joey Schoblaska <joey.schoblaska@gmail.com>
2019-05-31 10:31:36 -0500

    dz60 ESCFN layer

--- keyboards/dz60/keymaps/joey/keymap.c
+++ keyboards/dz60/keymaps/joey/keymap.c
@@ -6,6 +6,10 @@
 
 #define _______ KC_TRNS
 
+enum custom_keycodes {
+  ESCFN = SAFE_RANGE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Layer 0
@@ -14,20 +18,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |-----------------------------------------------------------------------------------------+
 * | Tab   |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \    |
 * |-----------------------------------------------------------------------------------------+
-* | Esc     |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+* | ESCFN   |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
 * |-----------------------------------------------------------------------------------------+
 * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  | Shift | RGBT |
 * |-----------------------------------------------------------------------------------------+
-* | Ctrl  |  Alt |  GUI |     Space     |  Fn1 |   Space    | Pscr | Fn1 |      |Rght |Down |
+* | Ctrl  |  Alt |  GUI |     Space     |  Fn1 |   Space    | Alt  | Fn1 |      |App | Ctrl |
 * `-----------------------------------------------------------------------------------------'
 */
 
 	LAYOUT_all(
 		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,  KC_5,    KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   _______, KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,   KC_E,  KC_R,    KC_T,    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
-		KC_ESC,           KC_A,    KC_S,   KC_D,  KC_F,    KC_G,    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
+		ESCFN,            KC_A,    KC_S,   KC_D,  KC_F,    KC_G,    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
 		KC_LSFT, _______, KC_Z,    KC_X,   KC_C,  KC_V,    KC_B,    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,  KC_RSFT, RGB_TOG,
-		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(1), KC_SPC,  KC_PSCR, MO(1), _______, KC_DOWN, KC_RGHT),
+		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(1), KC_SPC,  KC_RALT, MO(1), _______, KC_APP, KC_RCTL),
 
 
 
@@ -52,4 +56,91 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, BL_TOGG, BL_DEC,  BL_INC,  BL_STEP, _______, _______, _______, _______, KC_VOLU, _______,
 		_______, _______, _______, _______, _______, KC_MSTP, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT),
+};
+
+int escfn_down = 0;
+int escfn_mod_pressed = 0;
+int modded_h_pressed = 0;
+int modded_j_pressed = 0;
+int modded_k_pressed = 0;
+int modded_l_pressed = 0;
+int modded_o_pressed = 0;
+int modded_sc_pressed = 0;
+
+void modded_key_press(keyrecord_t *record, uint16_t orig, uint16_t modded, int *mod_pressed) {
+  if (record->event.pressed) {
+    if (escfn_down == 1) {
+      escfn_mod_pressed = 1;
+      *mod_pressed = 1;
+      register_code(modded);
+    } else {
+      register_code(orig);
+    }
+  } else {
+    if (*mod_pressed == 1) {
+      unregister_code(modded);
+    } else {
+      unregister_code(orig);
+    }
+    *mod_pressed = 0;
+  }
+}
+
+void modded_macro_press(keyrecord_t *record, uint16_t orig, char* macro, int *mod_pressed) {
+  if (record->event.pressed) {
+    if (escfn_down == 1) {
+      escfn_mod_pressed = 1;
+      *mod_pressed = 1;
+      send_string(macro);
+    } else {
+      register_code(orig);
+    }
+  } else {
+    if (*mod_pressed == 0) {
+      unregister_code(orig);
+    }
+    *mod_pressed = 0;
+  }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch(keycode) {
+    case ESCFN:
+      if (record->event.pressed) {
+        escfn_down = 1;
+      } else {
+        if (escfn_mod_pressed == 0) {
+          SEND_STRING(SS_TAP(X_ESCAPE));
+        }
+        escfn_down = 0;
+      }
+      escfn_mod_pressed = 0;
+      return false;
+
+    case KC_H:
+      modded_key_press(record, KC_H, KC_LEFT, &modded_h_pressed);
+      return false;
+
+    case KC_J:
+      modded_key_press(record, KC_J, KC_DOWN, &modded_j_pressed);
+      return false;
+
+    case KC_K:
+      modded_key_press(record, KC_K, KC_UP, &modded_k_pressed);
+      return false;
+
+    case KC_L:
+      modded_key_press(record, KC_L, KC_RIGHT, &modded_l_pressed);
+      return false;
+
+    case KC_O:
+      modded_macro_press(record, KC_O, SS_LCTRL("a")"o", &modded_o_pressed);
+      return false;
+
+    case KC_SCLN:
+      modded_macro_press(record, KC_SCLN, SS_LCTRL("a")";", &modded_sc_pressed);
+      return false;
+  }
+
+  return true;
 };
