b5f0d4bc338c1ac2eb962351eeeb62d7fe10c415
Joey Schoblaska <joey.schoblaska@gmail.com>
2019-05-31 17:14:15 -0500

    more dz60 tweaks

--- keyboards/dz60/keymaps/joey/keymap.c
+++ keyboards/dz60/keymaps/joey/keymap.c
@@ -1,13 +1,19 @@
 // disconnect keyboard
 // make dz60:joey:dfu
 // reconnect keyboard while holding space + b
+//
+// OR
+//
+// make dz60:joey:dfu
+// issue RESET command
 
 #include QMK_KEYBOARD_H
 
 #define _______ KC_TRNS
 
 enum custom_keycodes {
-  ESCFN = SAFE_RANGE
+  ESCFN = SAFE_RANGE,
+  LANG_TG
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -20,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |-----------------------------------------------------------------------------------------+
 * | ESCFN   |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
 * |-----------------------------------------------------------------------------------------+
-* | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  | Shift | RGBT |
+* | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  | / |  | Shift |Lang Tog|
 * |-----------------------------------------------------------------------------------------+
 * | Ctrl  |  Alt |  GUI |     Space     |  Fn1 |   Space    | Alt  | Fn1 |      |App | Ctrl |
 * `-----------------------------------------------------------------------------------------'
@@ -30,32 +36,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,  KC_5,    KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   _______, KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,   KC_E,  KC_R,    KC_T,    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
 		ESCFN,            KC_A,    KC_S,   KC_D,  KC_F,    KC_G,    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
-		KC_LSFT, _______, KC_Z,    KC_X,   KC_C,  KC_V,    KC_B,    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,  KC_RSFT, RGB_TOG,
+		KC_LSFT, _______, KC_Z,    KC_X,   KC_C,  KC_V,    KC_B,    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,  KC_RSFT, LANG_TG,
 		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(1), KC_SPC,  KC_RALT, MO(1), _______, KC_APP, KC_RCTL),
 
 
 
  /* Layer 1
 * ,-----------------------------------------------------------------------------------------.
-* | PWR | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |     | Del |
+* | ESC | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |     | Del |
 * |-----------------------------------------------------------------------------------------+
-* |       |RGBT |RGBM |Hue+ |Hue- |Sat+ |Sat- |Val+ |Val- |     |     |     |     |  RESET  |
+* |       |Prev |Play |Next |     |     |     |     |     |     |     |     |     |  RESET  |
 * |-----------------------------------------------------------------------------------------+
-* |         |     |     |     |     |     |     |     |     |     |     |     |             |
+* |         |     |Vol- |Vol+ |Mute |     |     |     |     |     |     |     |             |
 * |-----------------------------------------------------------------------------------------+
-* |           |     |     |     | BLT | BL- | BL+ | BLS |     |     |           |Vol+ |     |
+* |           |     |     |     |     |     |     |     |     |     |           |     |     |
 * |-----------------------------------------------------------------------------------------+
-* |      |      |      |                |      |    Stop     |      |     |Prev |Vol- |Next |
+* |      |      |      |                |      |             |      |     |     |     |     |
 * `-----------------------------------------------------------------------------------------'
 */
 
 
   LAYOUT_all(
-		KC_PWR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_DEL,
-		_______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,  RESET,
-		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, _______, _______, BL_TOGG, BL_DEC,  BL_INC,  BL_STEP, _______, _______, _______, _______, KC_VOLU, _______,
-		_______, _______, _______, _______, _______, KC_MSTP, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT),
+		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_DEL,
+		_______,          KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______,  RESET,
+		_______,          _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 };
 
 int escfn_down = 0;
@@ -139,6 +145,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case KC_SCLN:
       modded_macro_press(record, KC_SCLN, SS_LCTRL("a")";", &modded_sc_pressed);
+      return false;
+
+    case LANG_TG:
+      if (record->event.pressed) {
+        send_string(SS_LGUI(SS_LALT(" ")));
+      } else {
+        // key released
+      }
       return false;
   }
 
