4d001e786549ee0eb26efec4832a3f66973237a0
Joey Schoblaska <joey.schoblaska@gmail.com>
2019-05-31 20:09:11 -0500

    dz60 mid space and rgb tog

--- keyboards/dz60/keymaps/joey/keymap.c
+++ keyboards/dz60/keymaps/joey/keymap.c
@@ -28,16 +28,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |-----------------------------------------------------------------------------------------+
 * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  | / |  | Shift |Lang Tog|
 * |-----------------------------------------------------------------------------------------+
-* | Ctrl  |  Alt |  GUI |     Space     |  Fn1 |   Space    | Alt  | Fn1 |      |App | Ctrl |
+* | Ctrl  |  Alt |  GUI |     Space    | Space |   Space    | Alt  | Fn1 |      |App | Ctrl |
 * `-----------------------------------------------------------------------------------------'
 */
 
 	LAYOUT_all(
-		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,  KC_5,    KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   _______, KC_BSPC,
-		KC_TAB,           KC_Q,    KC_W,   KC_E,  KC_R,    KC_T,    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
-		ESCFN,            KC_A,    KC_S,   KC_D,  KC_F,    KC_G,    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
-		KC_LSFT, _______, KC_Z,    KC_X,   KC_C,  KC_V,    KC_B,    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,  KC_RSFT, LANG_TG,
-		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(1), KC_SPC,  KC_RALT, MO(1), _______, KC_APP, KC_RCTL),
+		KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   _______, KC_BSPC,
+		KC_TAB,           KC_Q,    KC_W,   KC_E,   KC_R,    KC_T,    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
+		ESCFN,            KC_A,    KC_S,   KC_D,   KC_F,    KC_G,    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
+		KC_LSFT, _______, KC_Z,    KC_X,   KC_C,   KC_V,    KC_B,    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,  KC_RSFT, LANG_TG,
+		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC,  KC_RALT, MO(1), _______, KC_APP, KC_RCTL),
 
 
 
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |-----------------------------------------------------------------------------------------+
 * |       |Prev |Play |Next |     |     |     |     |     |     |     |     |     |  RESET  |
 * |-----------------------------------------------------------------------------------------+
-* |         |     |Vol- |Vol+ |Mute |     |     |     |     |     |     |     |             |
+* |         |RGBTG|Vol- |Vol+ |Mute |     |     |     |     |     |     |     |             |
 * |-----------------------------------------------------------------------------------------+
 * |           |     |     |     |     |     |     |     |     |     |           |     |     |
 * |-----------------------------------------------------------------------------------------+
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT_all(
 		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_DEL,
 		_______,          KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______,  RESET,
-		_______,          _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______,          RGB_TOG, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 };
