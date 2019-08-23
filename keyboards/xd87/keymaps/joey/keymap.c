553bbffef594b38ce802aeefd604e2d7a01ef2f8
Joey Schoblaska <joey.schoblaska@gmail.com>
2019-08-23 12:20:58 -0500

    improved layout for xd87

--- keyboards/xd87/keymaps/joey/keymap.c
+++ keyboards/xd87/keymaps/joey/keymap.c
@@ -2,21 +2,24 @@
 // press key combo for RESET
 // or
 // disconnect and reconnect board while holding space + b
+//
+// note: may finish with "Error 254", but that may be OK
 
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-  ESCFN = SAFE_RANGE
+  ESCFN = SAFE_RANGE,
+  LANG_TG
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_tkl_split_shift(
       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,               KC_PSCR,  KC_SLCK,  KC_PAUS,
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,    KC_MPRV,  KC_MPLY,  KC_MNXT,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,              KC_MUTE,  KC_VOLD,  KC_VOLU,
       ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS,  KC_ENT,
-      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  KC_X,                 KC_UP,
+      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  LANG_TG,              KC_UP,
       KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                                      KC_RALT, MO(1),    KC_APP,   KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT  ),
 
   [1] = LAYOUT_tkl_split_shift(
@@ -24,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   RESET,      KC_INS,   KC_HOME,  KC_PGUP,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,
       ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS,  KC_ENT,
-      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  KC_X,                 KC_UP,
+      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  LANG_TG,              KC_UP,
       KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                                      KC_RALT, MO(1),    KC_APP,   KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT  )
 };
 
@@ -109,6 +112,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case KC_SCLN:
       modded_macro_press(record, KC_SCLN, SS_LCTRL("a")";", &modded_sc_pressed);
+      return false;
+
+    case LANG_TG:
+      if (record->event.pressed) {
+        send_string(SS_LGUI(SS_LALT(" ")));
+      } else {
+        // key released
+      }
       return false;
   }
 
