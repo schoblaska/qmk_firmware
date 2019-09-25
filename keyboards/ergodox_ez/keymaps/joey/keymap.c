f254091d2f2847849010eff432b7fcc1ac9bb3c8
Joey Schoblaska <joey.schoblaska@gmail.com>
2019-09-25 12:03:05 +0200

    more ergodox tweaks

--- keyboards/ergodox_ez/keymaps/joey/keymap.c
+++ keyboards/ergodox_ez/keymaps/joey/keymap.c
@@ -16,27 +16,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,
     ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,
-    KC_LCTL, KC_LALT, KC_LGUI, _______, _______,
-                                                 _______, _______,
-                                                          _______,
-                                        KC_LGUI,  MO(1),  _______,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,
+                                                 KC_MPRV, KC_MNXT,
+                                                          KC_MPLY,
+                                        KC_LGUI, KC_LCTL, _______,
 
                                                                     // right hand
                                                                     _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-                                                                    _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+                                                                    KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
                                                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-                                                                    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                                                                      _______, _______, _______, _______, _______,
-                                                                    _______, _______,
-                                                                    _______,
+                                                                    KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                                                                      _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,
+                                                                    KC_VOLD, KC_VOLU,
+                                                                    KC_MUTE,
                                                                     _______, MO(1),   KC_SPC
   ),
 
   [1] = LAYOUT_ergodox(
     // left hand
     _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______,
-    _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,
+    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,
     KC_LSFT, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,
                                                  _______, _______,
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         _______, _______, _______,
 
                                                                     // right hand
-                                                                    _______, _______, _______, _______, KC_MINS, KC_EQL,  _______,
-                                                                    _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,
-                                                                             _______, _______, _______, _______, KC_QUOT, _______,
+                                                                    _______, _______, _______, _______, _______, _______,  _______,
+                                                                    _______, _______, _______, _______, _______, _______, _______,
+                                                                             _______, _______, _______, _______, _______, _______,
                                                                     _______, _______, _______, _______, _______, _______, KC_RSFT,
                                                                                       _______, _______, _______, _______, _______,
                                                                     _______,    _______,
