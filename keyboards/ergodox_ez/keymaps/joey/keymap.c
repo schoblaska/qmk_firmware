7d2ea237d45c43bae2efa9ca8f524cc0b8497bee
Joey Schoblaska <joey.schoblaska@gmail.com>
2019-09-25 16:06:13 +0200

    more ergo tweaks

--- keyboards/ergodox_ez/keymaps/joey/keymap.c
+++ keyboards/ergodox_ez/keymaps/joey/keymap.c
@@ -4,6 +4,13 @@
 #include QMK_KEYBOARD_H
 
 #define _______ KC_TRNS
+#define DBLQUOT LSFT(KC_QUOT)
+#define LCURLY LSFT(KC_LBRC)
+#define RCURLY LSFT(KC_RBRC)
+#define LPAREN LSFT(KC_9)
+#define RPAREN LSFT(KC_0)
+#define UNDERSC LSFT(KC_MINS)
+#define PLUS LSFT(KC_EQL)
 
 enum custom_keycodes {
   ESCFN = SAFE_RANGE
@@ -29,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                       _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,
                                                                     KC_VOLD, KC_VOLU,
                                                                     KC_MUTE,
-                                                                    _______, MO(1),   KC_SPC
+                                                                    MO(2),   MO(1),   KC_SPC
   ),
 
   [1] = LAYOUT_ergodox(
@@ -44,14 +51,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         _______, _______, _______,
 
                                                                     // right hand
-                                                                    _______, _______, _______, _______, _______, _______,  _______,
                                                                     _______, _______, _______, _______, _______, _______, _______,
-                                                                             _______, _______, _______, _______, _______, _______,
-                                                                    _______, _______, _______, KC_UP,   _______, _______, KC_RSFT,
-                                                                                      KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-                                                                    _______,    _______,
+                                                                    _______, PLUS,    UNDERSC, LCURLY,  RCURLY,  DBLQUOT, _______,
+                                                                             KC_EQL,  KC_MINS, KC_LBRC, KC_RBRC, KC_QUOT, _______,
+                                                                    _______, _______, _______, LPAREN,  RPAREN,  _______, KC_RSFT,
+                                                                                      _______, _______, _______, _______, _______,
+                                                                    _______, _______,
                                                                     _______,
                                                                     _______, _______, _______
+  ),
+
+  [2] = LAYOUT_ergodox(
+    // left hand
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,
+    ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,
+                                                 KC_MPRV, KC_MNXT,
+                                                          KC_MPLY,
+                                        KC_LGUI, KC_SPC,  KC_LCTL,
+
+                                                                    // right hand
+                                                                    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+                                                                    KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+                                                                             KC_H,    KC_UP,   KC_K,    KC_L,    KC_SCLN, KC_ENT,
+                                                                    KC_EQL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_DOT,  KC_SLSH, KC_RSFT,
+                                                                                      _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,
+                                                                    KC_VOLD, KC_VOLU,
+                                                                    KC_MUTE,
+                                                                    _______, _______,   KC_SPC
   ),
 };
 
