3c8b43e6a6c02fda05ae9b41d92452900deaadf3
Joey Schoblaska <joey.schoblaska@gmail.com>
2019-09-27 16:09:19 -0500

    move some spaces around

--- keyboards/ergodox_ez/keymaps/joey/keymap.c
+++ keyboards/ergodox_ez/keymaps/joey/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,
     ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,
-    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,
+    KC_LCTL, KC_LALT, KC_LGUI, _______, KC_SPC,
                                                  KC_MPRV, KC_MNXT,
                                                           KC_MPLY,
                                         KC_LGUI, MO(1),   KC_LCTL,
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                     KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
                                                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
                                                                     KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                                                                      _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,
+                                                                                      KC_SPC,  _______, KC_LBRC, KC_RBRC, KC_QUOT,
                                                                     KC_VOLD, KC_VOLU,
                                                                     KC_MUTE,
                                                                     MO(2),   KC_RCTL, KC_SPC
