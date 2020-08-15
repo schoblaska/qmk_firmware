e07341bf4600b2f14800fe402b1bcae8bd9ac7f6
Joey Schoblaska <joey.schoblaska@gmail.com>
2020-08-15 09:40:13 -0500

    overhaul ergodox layout

--- keyboards/ergodox_ez/keymaps/joey/keymap.c
+++ keyboards/ergodox_ez/keymaps/joey/keymap.c
@@ -9,79 +9,90 @@
 #define RCURLY LSFT(KC_RBRC)
 #define LPAREN LSFT(KC_9)
 #define RPAREN LSFT(KC_0)
-#define UNDERSC LSFT(KC_MINS)
+#define UNDRSC LSFT(KC_MINS)
 #define PLUS LSFT(KC_EQL)
 #define CTRL_A LCTL(KC_A)
 
 enum custom_keycodes {
-  ESCFN = SAFE_RANGE
+  ESCFN = SAFE_RANGE,
+  LANG_TG
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox(
-    // left hand
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,
     ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,
     KC_LCTL, KC_LALT, KC_LGUI, _______, KC_SPC,
-                                                 KC_MPRV, KC_MNXT,
-                                                          KC_MPLY,
-                                        KC_LGUI, MO(1),   CTRL_A,
-
-                                                                    // right hand
+                                                 TG(3),   LANG_TG,
+                                                          _______,
+                                        KC_LGUI, MO(1),   _______,
                                                                     _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-                                                                    KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+                                                                    _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
                                                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-                                                                    KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                                                                      KC_SPC,  _______, KC_LBRC, KC_RBRC, KC_QUOT,
-                                                                    KC_VOLD, KC_VOLU,
-                                                                    KC_MUTE,
+                                                                    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                                                                      _______, _______, _______, _______, _______,
+                                                                    _______, _______,
+                                                                    _______,
                                                                     MO(2),   KC_RCTL, KC_SPC
   ),
 
   [1] = LAYOUT_ergodox(
-    // left hand
     _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,
-    KC_LSFT, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,
                                                  _______, _______,
                                                           _______,
                                         _______, _______, _______,
-
-                                                                    // right hand
-                                                                    _______, _______, _______, _______, _______, KC_MINS, KC_EQL,
-                                                                    _______, _______, PLUS,    LCURLY,  RCURLY,  DBLQUOT, _______,
-                                                                             _______, UNDERSC, KC_LBRC, KC_RBRC, KC_QUOT, _______,
-                                                                    _______, _______, _______, _______, _______, _______, KC_RSFT,
-                                                                                      _______, _______, _______, _______, _______,
+                                                                    _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_DEL,
+                                                                    _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_PGUP,
+                                                                             _______, _______, _______, _______, KC_QUOT, KC_PGDN,
+                                                                    _______, _______, _______, _______, _______, KC_UP,   _______,
+                                                                                      _______, _______, KC_LEFT, KC_DOWN, KC_RGHT,
                                                                     _______, _______,
                                                                     _______,
                                                                     _______, _______, _______
   ),
 
   [2] = LAYOUT_ergodox(
-    // left hand
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,
-    ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,
-    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,
-                                                 KC_MPRV, KC_MNXT,
-                                                          KC_MPLY,
-                                        KC_LGUI, KC_SPC,  KC_LCTL,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______,
+    _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,
+                                                 _______, _______,
+                                                          _______,
+                                        _______, _______, _______,
+                                                                    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+                                                                    _______, _______, _______, _______, _______, _______, KC_F12,
+                                                                             _______, _______, _______, _______, _______, _______,
+                                                                    _______, _______, _______, _______, _______, _______, _______,
+                                                                                      _______, _______, _______, _______, _______,
+                                                                    _______, _______,
+                                                                    _______,
+                                                                    _______, _______, _______
+  ),
 
-                                                                    // right hand
-                                                                    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-                                                                    KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-                                                                             KC_H,    KC_UP,   KC_K,    KC_L,    KC_SCLN, KC_ENT,
-                                                                    KC_EQL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_DOT,  KC_SLSH, KC_RSFT,
-                                                                                      _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,
-                                                                    KC_VOLD, KC_VOLU,
-                                                                    KC_MUTE,
-                                                                    _______, _______,   KC_SPC
+  [3] = LAYOUT_ergodox(
+    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,
+                                                 _______, _______,
+                                                          _______,
+                                        KC_SPC,  _______, _______,
+                                                                    _______, _______, _______, _______, _______, _______, _______,
+                                                                    _______, _______, _______, _______, _______, _______, _______,
+                                                                             _______, _______, _______, _______, _______, _______,
+                                                                    _______, _______, _______, _______, _______, _______, _______,
+                                                                                      _______, _______, _______, _______, _______,
+                                                                    _______, _______,
+                                                                    _______,
+                                                                    _______, _______, _______
   ),
 };
 
@@ -91,7 +102,11 @@ int modded_h_pressed = 0;
 int modded_j_pressed = 0;
 int modded_k_pressed = 0;
 int modded_l_pressed = 0;
+int modded_spc_pressed = 0;
+int modded_9_pressed = 0;
+int modded_0_pressed = 0;
 int modded_o_pressed = 0;
+int modded_p_pressed = 0;
 int modded_sc_pressed = 0;
 
 void modded_key_press(keyrecord_t *record, uint16_t orig, uint16_t modded, int *mod_pressed) {
@@ -160,12 +175,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       modded_key_press(record, KC_L, KC_RIGHT, &modded_l_pressed);
       return false;
 
+    case KC_SPC:
+      modded_macro_press(record, KC_SPC, SS_LCTRL("a")"o", &modded_spc_pressed);
+      return false;
+
+    case KC_9:
+      modded_key_press(record, KC_9, UNDRSC, &modded_9_pressed);
+      return false;
+
+    case KC_0:
+      modded_key_press(record, KC_0, PLUS, &modded_0_pressed);
+      return false;
+
     case KC_O:
-      modded_macro_press(record, KC_O, SS_LCTRL("a")"o", &modded_o_pressed);
+      modded_key_press(record, KC_O, LCURLY, &modded_o_pressed);
+      return false;
+
+    case KC_P:
+      modded_key_press(record, KC_P, RCURLY, &modded_p_pressed);
       return false;
 
     case KC_SCLN:
-      modded_macro_press(record, KC_SCLN, SS_LCTRL("a")";", &modded_sc_pressed);
+      modded_key_press(record, KC_SCLN, DBLQUOT, &modded_sc_pressed);
+      return false;
+
+    case LANG_TG:
+      if (record->event.pressed) {
+        send_string(SS_LGUI(SS_LALT(" ")));
+      } else {
+        // key released
+      }
       return false;
   }
 
