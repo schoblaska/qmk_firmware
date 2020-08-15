0f4921e804fcb6f5a037320ccaed49a3f47a3ec6
Joey Schoblaska <joey.schoblaska@gmail.com>
2020-08-15 09:31:51 -0500

    I finally realized how layers work...

--- keyboards/xd87/keymaps/joey/keymap.c
+++ keyboards/xd87/keymaps/joey/keymap.c
@@ -3,9 +3,11 @@
 // or
 // disconnect and reconnect board while holding space + b
 //
-// note: may finish with "Error 254", but that may be OK
+// note: may finish with "Error 254", but that is OK (I think?)
 
 #include QMK_KEYBOARD_H
+
+#define _______ KC_TRNS
 
 enum custom_keycodes {
   ESCFN = SAFE_RANGE,
@@ -15,28 +17,29 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_tkl_split_shift(
-      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,               KC_PSCR,  KC_SLCK,  KC_PAUS,
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
-      ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS,  KC_ENT,
-      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  LANG_TG,              KC_UP,
-      KC_LCTL, KC_LALT, KC_LGUI,          KC_LGUI, KC_SPC,                                      KC_RALT, MO(1),    DF(2),    KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT  ),
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,              KC_PSCR, KC_SLCK, KC_PAUS,
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,
+      ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
+      KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, LANG_TG,             KC_UP,
+      KC_LCTL, KC_LALT, KC_LGUI,          KC_LGUI, KC_SPC,                                      KC_RALT, MO(1),   TG(2),   KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT  ),
 
   [1] = LAYOUT_tkl_split_shift(
-      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,               KC_PSCR,  KC_SLCK,  KC_PAUS,
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   RESET,      KC_INS,   KC_HOME,  KC_PGUP,
-      KC_TAB,  KC_MPRV, KC_MPLY, KC_MNXT, KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
-      ESCFN,   KC_A,    KC_MUTE, KC_VOLD, KC_VOLU, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS,  KC_ENT,
-      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  LANG_TG,              KC_UP,
-      KC_LCTL, KC_LALT, KC_LGUI,          KC_LGUI, KC_SPC,                                      KC_RALT, MO(1),    DF(2),    KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT  ),
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,      _______, _______, _______,
+      _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+      _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______,
+      _______, _______, _______,          _______, _______,                                     _______, _______, _______, _______,    _______, _______, _______  ),
 
   [2] = LAYOUT_tkl_split_shift(
-      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,               KC_PSCR,  KC_SLCK,  KC_PAUS,
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
-      ESCFN,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS,  KC_ENT,
-      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  LANG_TG,              KC_UP,
-      KC_LCTL, KC_LALT, KC_LGUI,          KC_SPC,  KC_SPC,                                      KC_RALT, MO(1),    DF(0),    KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT  )
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______,
+      _______, _______, _______,          KC_SPC,  _______,                                     _______, _______, _______, _______,    _______, _______, _______  )
+
 };
 
 int escfn_down = 0;
@@ -45,8 +48,7 @@ int modded_h_pressed = 0;
 int modded_j_pressed = 0;
 int modded_k_pressed = 0;
 int modded_l_pressed = 0;
-int modded_o_pressed = 0;
-int modded_sc_pressed = 0;
+int modded_spc_pressed = 0;
 
 void modded_key_press(keyrecord_t *record, uint16_t orig, uint16_t modded, int *mod_pressed) {
   if (record->event.pressed) {
@@ -114,12 +116,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       modded_key_press(record, KC_L, KC_RIGHT, &modded_l_pressed);
       return false;
 
-    case KC_O:
-      modded_macro_press(record, KC_O, SS_LCTRL("a")"o", &modded_o_pressed);
-      return false;
-
-    case KC_SCLN:
-      modded_macro_press(record, KC_SCLN, SS_LCTRL("a")";", &modded_sc_pressed);
+    case KC_SPC:
+      modded_macro_press(record, KC_SPC, SS_LCTRL("a")"o", &modded_spc_pressed);
       return false;
 
     case LANG_TG:
