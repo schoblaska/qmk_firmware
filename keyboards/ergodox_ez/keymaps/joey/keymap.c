732d9af96ae858b82295f7424a2ac8904d95f250
Joey Schoblaska <joey.schoblaska@gmail.com>
2020-08-27 09:40:23 -0500

    redundant 5

--- keyboards/ergodox_ez/keymaps/joey/keymap.c
+++ keyboards/ergodox_ez/keymaps/joey/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  TG(3),   LANG_TG,
                                                           _______,
                                         GUIMOD,  MO(1),   CTRL_A,
-                                                                    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+                                                                    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
                                                                     _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
                                                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
                                                                     KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
